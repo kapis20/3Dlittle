ab1bb20cddbdcce07a26a6c20b71a598a5dae7a4
kapis20 <kacper.filip.sikorski@gmail.com>
2024-12-24 00:31:49 +0000

    seems like all needed packages are installed

---
+++ install_packages.sh
@@ -0,0 +1,4 @@
+pip install -q --use-deprecated=legacy-resolver tflite-model-maker
+pip install -q pycocotools
+pip install -q opencv-python-headless==4.1.2.30
+pip uninstall -y tensorflow && pip install -q tensorflow==2.8.0
