d8a0cb36018619f22127f3f4c824356f757c82ab
kapis20 <kacper.filip.sikorski@gmail.com>
2024-12-24 19:23:24 +0000

    debugging done, model runs fine

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ logging.set_verbosity(logging.ERROR)
 ########################
 # paths 
 ########################
-train_csv_path = "/home/kapis20/Projects/3D_new/3Dlittle/data/train/_annotations.csv"
+train_csv_path = "/home/kapis20/Projects/3D_new/3Dlittle/data/train/_annotationscopy.csv"
 valid_csv_path = "/home/kapis20/Projects/3D_new/3Dlittle/data/valid/_annotations.csv"
 test_csv_path = "/home/kapis20/Projects/3D_new/3Dlittle/data/test/_annotations.csv"
 
@@ -50,29 +50,21 @@ test_csv_path = "/home/kapis20/Projects/3D_new/3Dlittle/data/test/_annotations.c
 
 
 spec = model_spec.get('efficientdet_lite1')
-
+train_data, validation_data, test_data = object_detector.DataLoader.from_csv(train_csv_path)
 #train_data = object_detector.DataLoader.from_csv(train_csv_path)
-validation_data = object_detector.DataLoader.from_csv(valid_csv_path)
+#validation_data = object_detector.DataLoader.from_csv(valid_csv_path)
 #test_data = object_detector.DataLoader.from_csv(test_csv_path)
 
-#print(type(train_data))
-print(type(validation_data))  # Check the type of train_data
-print(validation_data)  # Check the content of train_data
+# #print(type(train_data))
+# print(type(train_data))  # Check the type of train_data
+# print(train_data)  # Check the content of train_data
 
+model = object_detector.create(
+    train_data, model_spec=spec, 
+    epochs=50, batch_size=10, 
+    train_whole_model=True, 
+    validation_data=validation_data)
 
-# model = object_detector.create(
-#     train_data=train_data,
-#     model_spec=spec,
-#     validation_data=validation_data,
-#     epochs=10,
-# )
-
-
-# model = object_detector.create(
-#     train_data=train_data,
-#     model_spec=spec,
-#     validation_data=validation_data,
-#     epochs=1,
-# )
-# # Evaluate the model
-# model.evaluate(test_data)
+model.evaluate(test_data)
+model.export(export_dir='/home/kapis20/Projects/3D_new/3Dlittle/')
+model.evaluate_tflite('model.tflite', test_data)
